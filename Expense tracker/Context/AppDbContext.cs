ab7eba9218d374cd052c9ad5bcb2b9cddc9a8213
Maruf Berdiyev <93835738+fuga02@users.noreply.github.com>
2023-07-09 00:46:02 -0700

    Add project files.

---
+++ Expense tracker/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Expense_tracker.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_tracker.Context;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions options):base(options)
+    {
+
+    }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
